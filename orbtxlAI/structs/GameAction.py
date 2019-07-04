c22148e96d33ebc68456dd0170f5b92e8247d228
clarkwkw <clarkwkw@yahoo.com.hk>
2019-07-04 13:07:39 +0800

    add GameAction serialization

--- orbtxlAI/structs/GameAction.py
+++ orbtxlAI/structs/GameAction.py
@@ -2,3 +2,16 @@ class GameAction:
     def __init__(self, press_time, wait_time_after_press):
         self.press_time = press_time
         self.wait_time_after_press = wait_time_after_press
+
+    def serialize(self):
+        return {
+            'press_time': self.press_time,
+            'wait_time_after_press': self.wait_time_after_press
+        }
+
+    @classmethod
+    def deserialize(cls, d):
+        return cls(
+            press_time=d['press_time'],
+            wait_time_after_press=d['wait_time_after_press']
+        )
